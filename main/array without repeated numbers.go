10db9ea33a3f95f7005aee017325aad2b0b5b4d3
Sergij2 <sergijhich@gmail.com>
2021-05-13 21:18:57 +0300

    add file

---
+++ main/array without repeated numbers.go
@@ -0,0 +1,15 @@
+package main
+
+func arrayWithoutRepeatNumbers(slice []int) []int {
+	mapSlice := make(map[int]int) // створення мапи  для подальшого заповннея її з елементів вхідного слайсу
+	var resultSlice []int
+	for _, val := range slice {
+		if _, ok := mapSlice[val]; ok { //перевірка умови повторення елементів
+			mapSlice[val] += 1
+		} else {
+			mapSlice[val] = 0
+			resultSlice = append(resultSlice, val)
+		}
+	}
+	return resultSlice
+}
